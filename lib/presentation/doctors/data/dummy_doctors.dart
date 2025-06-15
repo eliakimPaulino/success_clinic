2e0cef4548105d3651ac08fcb987c87785fe0073
eliakim <eliakim.paulino@gmail.com>
2025-06-15 14:58:28 -0300

    fix: update doctor image paths in dummy_doctors.dart and clean up asset declarations in pubspec.yaml

--- lib/presentation/doctors/data/dummy_doctors.dart
+++ lib/presentation/doctors/data/dummy_doctors.dart
@@ -4,26 +4,26 @@ final List<Medico> dummyDoctors = [
   Medico(
     nome: 'William Rangel',
     especialidade: 'Endocrinologista',
-    photo: 'assets/dr_willian_rangel.png',
+    photo: 'assets/images/doctors_images/dr_willian_rangel.png',
   ),
   Medico(
     nome: 'Pedro Candelario',
     especialidade: 'Endocrinologista',
-    photo: 'assets/pedro_candelario.png',
+    photo: 'assets/images/doctors_images/pedro_candelario.png',
   ),
   Medico(
     nome: 'Rafael Cardoso',
     especialidade: 'Endocrinologista',
-    photo: 'assets/rafael_cardoso.png',
+    photo: 'assets/images/doctors_images/rafael_cardoso.png',
   ),
   Medico(
     nome: 'Yandra Guerra',
     especialidade: 'Nutricionista',
-    photo: 'assets/yandra_guera.png',
+    photo: 'assets/images/doctors_images/yandra_guera.png',
   ),
   Medico(
     nome: 'Eliana Monoel',
     especialidade: 'Nutricionista',
-    photo: 'assets/eliane_manoel.png',
+    photo: 'assets/images/doctors_images/eliane_manoel.png',
   ),
 ];
