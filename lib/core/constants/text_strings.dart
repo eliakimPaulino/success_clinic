62e7e310cc5d777a9d9e9cb812c2b8e3e1c5978a
eliakim <eliakim.paulino@gmail.com>
2025-06-16 13:26:45 -0300

    feat: Add core utility classes for text constants, exceptions, and helpers
    - Introduced TTexts class for global text constants used throughout the app.
    - Created TFirebaseAuthException and TFirebaseException classes for handling Firebase-related errors.
    - Added TExceptions, TFormatException, TPlatformException for general error handling and format validation.
    - Implemented THttpHelper for simplified HTTP requests.
    - Developed TLocalStorage for local data storage management.
    - Added TLoggerHelper for logging functionalities.
    - Created TValidator for input validation including email, password, and phone number.
    - Introduced various helper functions for network management, pricing calculations, and UI state checks.

---
+++ lib/core/constants/text_strings.dart
@@ -0,0 +1,69 @@
+/// Esta classe contém todos os textos do aplicativo em formato String.
+class TTexts {
+
+  // -- Textos GLOBAIS
+  static const String and = "and";
+  static const String skip = "Pular";
+  static const String done = "Concluído";
+  static const String submit = "Enviar";
+  static const String appName = "T-Store";
+  static const String tContinue = "Continuar";
+
+
+  // -- Textos de OnBoarding
+  static const String onBoardingTitle1 = "Escolha seu produto";
+  static const String onBoardingTitle2 = "Selecione o método de pagamento";
+  static const String onBoardingTitle3 = "Entregue na sua porta";
+
+  static const String onBoardingSubTitle1 = "Bem-vindo a um mundo de escolhas ilimitadas - Seu produto perfeito espera por você!";
+  static const String onBoardingSubTitle2 = "Para transações sem complicações, escolha seu caminho de pagamento - Sua conveniência, nossa prioridade!";
+  static const String onBoardingSubTitle3 = "Da nossa porta para a sua - Entrega rápida, segura e sem contato!";
+
+
+  // -- Formulários de Autenticação
+  static const String firstName = "Nome";
+  static const String lastName = "Sobrenome";
+  static const String email = "E-mail";
+  static const String password = "Senha";
+  static const String newPassword = "Nova Senha";
+  static const String username = "Nome de usuário";
+  static const String phoneNo = "Número de telefone";
+  static const String rememberMe = "Lembrar-me";
+  static const String forgetPassword = "Esqueceu a senha?";
+  static const String signIn = "Entrar";
+  static const String createAccount = "Criar conta";
+  static const String orSignInWith = "ou entre com";
+  static const String orSignUpWith = "ou cadastre-se com";
+  static const String iAgreeTo = "Eu concordo com";
+  static const String privacyPolicy = "Política de Privacidade";
+  static const String termsOfUse = "Termos de uso";
+  static const String verificationCode = "Código de verificação";
+  static const String resendEmail = "Reenviar e-mail";
+  static const String resendEmailIn = "Reenviar e-mail em";
+
+
+  // -- Títulos de Autenticação
+  static const String loginTitle = "Bem-vindo de volta,";
+  static const String loginSubTitle = "Descubra escolhas ilimitadas e conveniência incomparável.";
+  static const String signupTitle = "Vamos criar sua conta";
+  static const String forgetPasswordTitle = "Esqueceu a senha";
+  static const String forgetPasswordSubTitle = "Não se preocupe, às vezes as pessoas esquecem mesmo. Digite seu e-mail e enviaremos um link para redefinir sua senha.";
+  static const String changeYourPasswordTitle = "E-mail de redefinição de senha enviado";
+  static const String changeYourPasswordSubTitle = "A segurança da sua conta é nossa prioridade! Enviamos um link seguro para você alterar sua senha e manter sua conta protegida.";
+  static const String confirmEmail = "Verifique seu endereço de e-mail!";
+  static const String confirmEmailSubTitle = "Parabéns! Sua conta está pronta: verifique seu e-mail para começar a comprar e aproveitar ofertas exclusivas e personalizadas.";
+  static const String emailNotReceivedMessage = "Não recebeu o e-mail? Verifique sua caixa de spam ou reenvie.";
+  static const String yourAccountCreatedTitle = "Sua conta foi criada com sucesso!";
+  static const String yourAccountCreatedSubTitle = "Bem-vindo ao seu destino definitivo de compras: sua conta foi criada, aproveite a experiência de compras online sem complicações!";
+
+
+  // -- Produto
+  static const String popularProducts = "Produtos populares";
+
+  // -- Home
+  static const String homeAppbarTitle = "Bom dia para compras";
+  static const String homeAppbarSubTitle = "Taimoor Sikander";
+
+
+}
+
